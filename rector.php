d2ecff4d43b0b4b19e62963455e469e3a9e63cd7
emersoncarvalho <carvemerson@gmail.com>
2025-04-13 16:23:10 -0300

    Update Rector config and enhance composer setup
    Standardize Rector config formatting for consistency. Move and expand `require-dev` dependencies in `composer.json`, and add detailed scripts for linting, refactoring, type checks, and unit tests using Pest and Larastan.

--- rector.php
+++ rector.php
@@ -6,9 +6,9 @@ use Rector\Config\RectorConfig;
 
 return RectorConfig::configure()
     ->withPaths([
-        __DIR__ . '/src',
-        __DIR__ . '/tests',
-        __DIR__ . '/workbench',
+        __DIR__.'/src',
+        __DIR__.'/tests',
+        __DIR__.'/workbench',
     ])
     ->withPreparedSets(
         deadCode: true,
